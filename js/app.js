1af60b98a9996d193a3f99d640ff520b116048f1
James Harvey <james.harvey@h2consulting.co.uk>
2023-01-17 23:27:02 +0000

    new file:   css/style.css 	new file:   css/style.css.map 	new file:   css/style.scss 	new file:   img/border-navigation.svg 	new file:   img/chevron-back-sharp.svg 	new file:   img/chevron-forward-sharp.svg 	new file:   img/dark-favicon.ico 	new file:   img/logo-final.png 	new file:   img/logo.svg 	new file:   img/profile-1.jpg 	new file:   img/profile-2.png 	new file:   img/profile-3.png 	new file:   img/profile-4.jpg 	new file:   img/st-peters.jpg 	new file:   img/student-squared-logo.svg 	new file:   index.html 	new file:   js/app.js

---
+++ js/app.js
@@ -0,0 +1,86 @@
+introAnimation();
+
+let count = 0;
+
+const sectionIds = ["#home-button", "#about-us-button", "#contact-us-button", "#our-tutors-button"];
+
+const mainSections = $(".main-section-item");
+const descriptionSections = $(".description-section-item");
+
+gsap.set(mainSections, { xPercent: 100 });
+gsap.set(mainSections[0], { xPercent: 0 });
+gsap.set(descriptionSections, { xPercent: 100 });
+gsap.set(descriptionSections[0], { xPercent: 0 });
+
+function introAnimation() {
+	const timeline = gsap.timeline({
+		onComplete: () => {
+			clearInterval(blinkCaret);
+			gsap.to(".typewriter-text", { borderColor: "transparent", duration: 1, ease: "power1.out" });
+			gsap.to(".intro-fade-in", { opacity: 1, duration: 3, ease: "power3.in" });
+		},
+	});
+	blinkCaret = setInterval(() => {
+		gsap.fromTo(".typewriter-text", { borderColor: "transparent" }, { borderColor: "#e9e9e9", duration: 0.1, ease: "linear" });
+	}, 500);
+	timeline.fromTo(".typewriter-text", { width: 0 }, { width: "100%", duration: 2.5, ease: "steps(14)" });
+}
+
+$(".menu-item").click(function () {
+	$(".menu-item").removeClass("active");
+	$(this).addClass("active");
+	let oldCount = count;
+	count = compareIds();
+	if (oldCount > count) {
+		gsap.fromTo(mainSections[count], { xPercent: -100 }, { xPercent: 0, duration: 1.2, ease: "power1.out" });
+		gsap.fromTo(mainSections[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: 100, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: 100, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[count], { xPercent: -100 }, { xPercent: 0, duration: 1.2, ease: "power1.out" });
+	} else if (oldCount < count) {
+		gsap.fromTo(mainSections[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
+		gsap.fromTo(mainSections[count], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[count], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
+	}
+});
+
+$(".right-arrow").click(function () {
+	if (count < mainSections.length - 1) {
+		animate("right");
+		count++;
+		$(".active").removeClass("active");
+		$(sectionIds[count]).addClass("active");
+	}
+});
+
+$(".left-arrow").click(function () {
+	if (count > 0) {
+		animate("left");
+		count--;
+		$(".active").removeClass("active");
+		$(sectionIds[count]).addClass("active");
+	}
+});
+
+function animate(dir) {
+	if (dir == "left") {
+		gsap.fromTo(mainSections[count - 1], { xPercent: -100 }, { xPercent: 0, duration: 1.2, ease: "power1.out" });
+		gsap.fromTo(mainSections[count], { xPercent: 0 }, { duration: 1.2, xPercent: 100, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[count], { xPercent: 0 }, { duration: 1.2, xPercent: 100, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[count - 1], { xPercent: -100 }, { xPercent: 0, duration: 1.2, ease: "power1.out" });
+	} else if (dir == "right") {
+		gsap.fromTo(mainSections[count], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
+		gsap.fromTo(mainSections[count + 1], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[count + 1], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
+		gsap.fromTo(descriptionSections[count], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
+	}
+}
+
+function compareIds() {
+	currentId = "#" + $(".active").attr("id");
+	for (let i = 0; i < sectionIds.length; i++) {
+		if (currentId == sectionIds[i]) {
+			return i;
+		}
+	}
+}

67235fd8312e6dbb13c06586a3ea8bab5747b6a7
James Harvey <james.harvey@h2consulting.co.uk>
2023-01-25 18:04:44 +0000

    Changes to be committed: 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   css/style.scss 	new file:   css/variable-sizing.css 	new file:   css/variable-sizing.css.map 	new file:   css/variable-sizing.scss 	deleted:    img/border-navigation.svg 	new file:   img/student-squared-logo-single.svg 	modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,19 @@ introAnimation();
 
 let count = 0;
 
-const sectionIds = ["#home-button", "#about-us-button", "#contact-us-button", "#our-tutors-button"];
+const sectionIds = ["#home-button", "#faq-button", "#contact-us-button", "#our-tutors-button"];
+const sectionIdsMobile = ["#home-button-mobile", "#faq-button-mobile", "#contact-us-button-mobile", "#our-tutors-button-mobile"];
 
 const mainSections = $(".main-section-item");
+const mainSectionsMobile = $(".main-section-item-mobile");
 const descriptionSections = $(".description-section-item");
 
 gsap.set(mainSections, { xPercent: 100 });
 gsap.set(mainSections[0], { xPercent: 0 });
+
+gsap.set(mainSectionsMobile, { xPercent: 100 });
+gsap.set(mainSectionsMobile[0], { xPercent: 0 });
+
 gsap.set(descriptionSections, { xPercent: 100 });
 gsap.set(descriptionSections[0], { xPercent: 0 });
 
@@ -41,6 +47,24 @@ $(".menu-item").click(function () {
 		gsap.fromTo(mainSections[count], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
 		gsap.fromTo(descriptionSections[count], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
 		gsap.fromTo(descriptionSections[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
+	}
+});
+
+$(".mobile-nav").click(function () {
+	$(".mobile-nav").removeClass("active-mobile");
+	$(this).addClass("active-mobile");
+	let oldCount = count;
+	count = compareIdsMobile();
+	if (oldCount > count) {
+		gsap.fromTo(mainSectionsMobile[count], { xPercent: -100 }, { xPercent: 0, duration: 1.2, ease: "power1.out" });
+		gsap.fromTo(mainSectionsMobile[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: 100, ease: "power1.out" });
+		gsap.fromTo(descriptionSectionsMobile[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: 100, ease: "power1.out" });
+		gsap.fromTo(descriptionSectionsMobile[count], { xPercent: -100 }, { xPercent: 0, duration: 1.2, ease: "power1.out" });
+	} else if (oldCount < count) {
+		gsap.fromTo(mainSectionsMobile[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
+		gsap.fromTo(mainSectionsMobile[count], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
+		gsap.fromTo(descriptionSectionsMobile[count], { xPercent: 100 }, { duration: 1.2, xPercent: 0, ease: "power1.out" });
+		gsap.fromTo(descriptionSectionsMobile[oldCount], { xPercent: 0 }, { duration: 1.2, xPercent: -100, ease: "power1.out" });
 	}
 });
 
@@ -85,26 +109,11 @@ function compareIds() {
 	}
 }
 
-function sendEmail() {
-	let body =
-		"Name: " +
-		$("#first-name").value +
-		" " +
-		$("#last-name").value +
-		"<br> Email: " +
-		$("#email").value +
-		"<br> Telephone: " +
-		$("#tel").value +
-		"<br> Message: " +
-		$("#text-area").value;
-
-	Email.send({
-		Host: "smtp.gmail.com",
-		Username: "spam.jamesharvey@gmail.com",
-		Password: "hauxley01",
-		To: "james.harvey@h2consulting.co.uk",
-		From: document.getElementById("email").value,
-		Subject: "New Enquiry",
-		Body: body,
-	}).then((message) => alert(message));
+function compareIdsMobile() {
+	currentId = "#" + $(".active-mobile").attr("id");
+	for (let i = 0; i < sectionIdsMobile.length; i++) {
+		if (currentId == sectionIdsMobile[i]) {
+			return i;
+		}
+	}
 }
